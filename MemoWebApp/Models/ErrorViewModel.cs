871eb755cb0b5d8fe9a7832853968395282d4e4b
Namiki Kazuma <namityo@hotmail.com>
2019-10-22 14:26:19 +0900

    New ASP.NET Core MVC Project

---
+++ MemoWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MemoWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
